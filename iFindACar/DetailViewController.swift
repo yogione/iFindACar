be21ac0ca9c38159be311116da69cba736cae741
Srini Motheram <srini@srinimoeramsmbp.wowway.com>
2017-04-24 18:59:01 -0400

    monday 7 pm

--- iFindACar/DetailViewController.swift
+++ iFindACar/DetailViewController.swift
@@ -20,15 +20,22 @@ class DetailViewController: UIViewController, UINavigationControllerDelegate  {
     @IBOutlet var modelLabel2        :UILabel!
     @IBOutlet var mpgLabel           :UILabel!
     @IBOutlet var trannyLabel           :UILabel!
-    @IBOutlet var vehicleClassLabel           :UILabel!
+    @IBOutlet var vehicleClassLabel      :UILabel!
+    @IBOutlet var safetyRatingLabel      :UILabel!
+    @IBOutlet var carImageView           :UIImageView!
     
     var hostName4options :String!
     var hostName4detailsByVehicleId :String!
     var vehicleId        :String?
-    var powerTrain    :String!; var cityMPG1 :String!; var hwyMPG1 :String!; var vehicleClass :String!
+    var powerTrain   :String!; var cityMPG1 :String!; var hwyMPG1 :String!; var vehicleClass :String!; var safetyRating :String!
+    var imageurl    :String!
     var vehicleIDArray = [String]()
+    var vehicleIDArrayNHTSA = [String]()
     
-    // Get vehicle id from fuel economy.gov given make, model and year
+    var hostName4NHTSAVid :String!
+    var hostName4NHTSAByVehicleId :String!
+    
+    //MARK: - fuel economy.gov methods - given make, model and year
     func alamoFire2GetVID(currCar: Vehicle){
         print("in func alamoFire2GetVID")
        hostName4options = "http://www.fueleconomy.gov/ws/rest/vehicle/menu/options?" + "year=" + currCar.modelYear + "&make=" + currCar.make + "&model="+currCar.model
@@ -53,7 +60,6 @@ class DetailViewController: UIViewController, UINavigationControllerDelegate  {
                }
         }
     }
-    
     
     func alamoFire3ToGetMPG(){
         print("from alamoFire3ToGetMPG: \(vehicleIDArray.count)")
@@ -87,6 +93,72 @@ class DetailViewController: UIViewController, UINavigationControllerDelegate  {
             
             }
         }
+    //MARK:- NHTSA methods
+    
+    func getNHTSAdataVID(currCar: Vehicle){
+        print("in func getNHTSAdataVID")
+        hostName4NHTSAVid = "https://one.nhtsa.gov/webapi/api/SafetyRatings/modelyear/" + currCar.modelYear + "/make/" + currCar.make + "/model/" + currCar.model + "?format=xml"
+        print("\(hostName4NHTSAVid!)")
+        Alamofire.request(hostName4NHTSAVid, method: .get, parameters: nil).response { ( response) in
+           //  print(response.data!) // if you want to check XML data in debug window.
+            let xmlResult = SWXMLHash.parse(response.data!)
+            for elem in xmlResult["Response"]["Results"]["CrashTestedVehicle"].all {
+                  print(elem["VehicleId"].element!.text!)
+                guard let currVehicleId = elem["VehicleId"].element!.text  // ! as? String
+                    else {  continue }
+                self.vehicleIDArrayNHTSA.append(currVehicleId)
+                print("nthsa array count: \(self.vehicleIDArrayNHTSA.count)")
+                self.hostName4NHTSAByVehicleId = "https://one.nhtsa.gov/webapi/api/SafetyRatings/VehicleId/" + self.vehicleIDArrayNHTSA.first! + "?format=xml"
+                print("hostName4NHTSAByVehicleId: \(self.hostName4NHTSAByVehicleId!)")
+            }
+            DispatchQueue.main.async {
+                if self.vehicleIDArrayNHTSA.count > 0 {
+                   self.getSafetyRatingAndImageFromNHTSA()
+                }
+            }
+        }
+    }
+
+    func getSafetyRatingAndImageFromNHTSA(){
+        print("from getSafetyRatingAndImageFromNHTSA: \(vehicleIDArrayNHTSA.count)")
+        
+       self.hostName4NHTSAByVehicleId = "https://one.nhtsa.gov/webapi/api/SafetyRatings/VehicleId/" + self.vehicleIDArrayNHTSA.first! + "?format=xml"
+        Alamofire.request(self.hostName4NHTSAByVehicleId!, method: .get, parameters: nil).response { ( response) in
+            // print(response.data!) // if you want to check XML data in debug window.
+            let xmlResultFinal = SWXMLHash.parse(response.data!)
+            for elem in xmlResultFinal["Response"]["Results"]["SafetyRatingAssessment"].all {
+                //  print(elem["text"].element!.text!)
+                guard let overallRating1 = elem["OverallRating"].element!.text
+                    else { continue }
+                guard let imageurl1 = elem["VehiclePicture"].element!.text
+                    else { continue }
+                
+                
+                print("overall rating: \(overallRating1), \(imageurl1)")
+                self.safetyRating = overallRating1
+                self.imageurl = imageurl1
+                
+                
+            }
+            self.showCarDetails(currCar: self.currentVehicle!)
+            self.safetyRatingLabel.text =  "safety rating stars: " + "\(self.safetyRating!)"
+            
+          //  self.carImageView.setImageFromURl(stringImageUrl: "http://www.safercar.gov/staticfiles/DOT/safercar/ncapmedia/images/2017/v09969P084.jpg")
+            
+//            let url = NSURL(string:self.imageurl)
+//            let data = NSData(contentsOf:url! as URL)
+//            
+//            // It is the best way to manage nil issue.
+//            if (data?.length)! > 0 {
+//                self.carImageView.image = UIImage(data:data! as Data)
+//            } else {
+//                // In this when data is nil or empty then we can assign a placeholder image
+//                self.carImageView.image = UIImage(named: "http://www.safercar.gov/staticfiles/DOT/safercar/ncapmedia/images/2017/v09969P084.jpg")
+//            }
+            
+            
+        }
+    }
     
     func showCarDetails(currCar: Vehicle) {
         modelYearLabel2.text = currCar.modelYear
@@ -97,8 +169,11 @@ class DetailViewController: UIViewController, UINavigationControllerDelegate  {
     override func viewDidLoad() {
         super.viewDidLoad()
         alamoFire2GetVID(currCar: currentVehicle!)
+        DispatchQueue.main.async {
+            self.getNHTSAdataVID(currCar: self.currentVehicle!)
+        }
         showCarDetails(currCar: currentVehicle!)
-       
+        
     }
 
     override func viewDidAppear(_ animated: Bool) {
@@ -118,6 +193,18 @@ class DetailViewController: UIViewController, UINavigationControllerDelegate  {
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+    }
+}
+
+extension UIImageView{
+    
+    func setImageFromURl(stringImageUrl url: String){
+        
+        if let url = NSURL(string: url) {
+            if let data = NSData(contentsOf: url as URL) {
+                self.image = UIImage(data: data as Data)
+            }
+        }
     }
 }
 
